586b49e23072b412c9982caecf39d550da3dba69
adriangs1996 <adriangonzalezsanchez1996@gmail.com>
2022-04-18 11:11:48 -0400

    Add hability to read config from 'config.json' file

--- winter/settings.py
+++ winter/settings.py
@@ -1,4 +1,22 @@
+import json
+from pathlib import Path
+from typing import Any, Dict
 import pydantic as pdc
+
+
+def json_config_settings_source(settings: pdc.BaseSettings) -> Dict[str, Any]:
+    """
+    A simple settings source that loads variables from a JSON file
+    at the project's root.
+
+    Here we happen to choose to use the `env_file_encoding` from Config
+    when reading `config.json`
+    """
+    encoding = settings.__config__.env_file_encoding
+    try:
+        return json.loads(Path("config.json").read_text(encoding))
+    except FileNotFoundError:
+        return {}
 
 
 class ConnectionOptions(pdc.BaseSettings):
@@ -10,4 +28,16 @@ class ConnectionOptions(pdc.BaseSettings):
 
 class WinterSettings(pdc.BaseSettings):
     backend: str = "winter.drivers"
-    options: ConnectionOptions = ConnectionOptions()
+    connection_options: ConnectionOptions = ConnectionOptions()
+
+    class Config:
+        env_file_encoding = "utf-8"
+
+        @classmethod
+        def customise_sources(cls, init_settings, env_settings, file_secret_settings):
+            return (
+                init_settings,
+                json_config_settings_source,
+                env_settings,
+                file_secret_settings,
+            )
