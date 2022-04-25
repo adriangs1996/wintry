20345084ed2e5802ad74002e0a448d60538a6037
Adrian Gonzalez <adriangonzalezsanchez1996@gmail.com>
2022-04-24 22:00:18 -0400

    Move parsing logic to query time

--- winter/repository/base.py
+++ winter/repository/base.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Any, Callable, List, Optional, Type, TypeVar
+from typing import Any, Callable, Coroutine, List, Optional, Type, TypeVar
 import inspect
 from pydantic import BaseModel
 
@@ -14,16 +14,20 @@ class RepositoryError(Exception):
 T = TypeVar("T", bound=BaseModel)
 TDecorated = TypeVar("TDecorated")
 
+RuntimeDecorator = Callable[[Type[TDecorated]], Type[TDecorated]]
+RuntimeParsedMethod = partial[Any], partial[Coroutine[Any, Any, Any]]
+Func = Callable[..., Any]
 
-def is_processable(method: Callable):
+
+def is_processable(method: Callable[..., Any]) -> bool:
     return method.__name__ != "__init__" and not getattr(method, "_raw_method", False)
 
 
-def marked(method):
+def marked(method: Callable[..., Any]) -> bool:
     return not getattr(method, "_raw_method", False)
 
 
-def map_result_to_entity(entity, result):
+def map_result_to_entity(entity: Type[T], result: List[Any] | Any | None) -> List[T] | T | None:
     if isinstance(result, list):
         try:
             return [entity.from_orm(instance) for instance in result]
@@ -42,49 +46,70 @@ def map_result_to_entity(entity, result):
             return result
 
 
-def repository(entity: Type[T], table_name: Optional[str] = None, dry: bool = False):
-    target_name = table_name or f"{entity.__name__}s".lower()
+def repository(
+    entity: Type[T], table_name: Optional[str] = None, dry: bool = False
+) -> RuntimeDecorator[TDecorated]:
+    """
+    Convert a class into a repository (basically an object factory) of `entity`.
+    Methods not marked with :func:`raw_method` will be compiled and processed by
+    the winter engine to automatically generate a query for the given function name.
 
+    This resembles JPA behaviour, but `entity` is not enforced to contain any DB
+    information. In fact, is possible to create a `Backend` based on Python's
+    built-in `Set` type, so in-memory testing is posible.
+
+    Repository classes are can be used with MongoDB, or any relational DB supported
+    by SQLAlchemy. `entity` does not need to fulfill any special rule, but if it is 
+    recomended that it derives from `pydantic.BaseModel`.
+
+    Example
+    =======
+
+    >>> class User(BaseModel):
+    >>>     id: int
+    >>>     name: str
+    >>>
+    >>> @repository(User)
+    >>> class UserRepository:
+    >>>     async def get_by_id(self, *, id: int) -> User | None:
+    >>>         ...
+    >>>
+    >>> repo = UserRepository()
+    >>> loop = asyncio.get_event_loop()
+    >>> user = loop.run_until_complete(repo.get_by_id(id=2)) # It works! And if an user exists, it automatically retrieves an `User` instance. Use MongoDB by default
+
+    """
     def _runtime_method_parsing(cls: Type[TDecorated]) -> Type[TDecorated]:
-        def _getattribute(self, __name: str):
+        if getattr(entity, __SQL_ENABLED_FLAG__, False):
+            target_name = __mapper__[entity]
+        else:
+            target_name = table_name or f"{entity.__name__}s".lower()  # type: ignore
+
+        def _getattribute(self: Any, __name: str) -> Any:
             attr = super(cls, self).__getattribute__(__name)  # type: ignore
+            new_attr = _parse_function_name(__name, attr, target_name, dry)
 
-            def wrapper(*args, **kwargs):
-                result = attr(*args, **kwargs)
+            def wrapper(*args: Any, **kwargs: Any) -> List[T] | T | None:
+                result = new_attr(*args, **kwargs)
                 return map_result_to_entity(entity, result)
 
-            async def async_wrapper(*args, **kwargs):
-                result = await attr(*args, **kwargs)
+            async def async_wrapper(*args: Any, **kwargs: Any) -> List[T] | T | None:
+                result = await new_attr(*args, **kwargs)
                 return map_result_to_entity(entity, result)
 
-            if isinstance(attr, partial) and not dry:
-                if inspect.iscoroutinefunction(attr.func):
+            if isinstance(new_attr, partial):
+                if inspect.iscoroutinefunction(new_attr.func):
                     return async_wrapper
                 else:
                     return wrapper
+            elif inspect.iscoroutinefunction(new_attr):
+                return async_wrapper
+            elif inspect.isfunction(new_attr):
+                return wrapper
             else:
                 return attr
 
-        function_members = inspect.getmembers(cls, inspect.isfunction)
-
-        for fname, fobject in function_members:
-            if is_processable(fobject):
-                if inspect.iscoroutinefunction(fobject):
-                    if getattr(entity, __SQL_ENABLED_FLAG__, False):
-                        table_schema = __mapper__[entity]
-                        new_method = Backend.run_async(fname, table_schema, dry_run=dry)
-                    else:
-                        new_method = Backend.run_async(fname, target_name, dry_run=dry)
-                else:
-                    if getattr(entity, __SQL_ENABLED_FLAG__, False):
-                        table_schema = __mapper__[entity]
-                        new_method = Backend.run(fname, table_schema, dry_run=dry)
-                    else:
-                        new_method = Backend.run(fname, target_name, dry_run=dry)
-
-                setattr(new_method, "_raw_method", True)
-                setattr(cls, fname, new_method)
-        cls.__getattribute__ = _getattribute
+        cls.__getattribute__ = _getattribute  # type: ignore
 
         return cls
 
@@ -99,3 +124,13 @@ def raw_method(method: FuncT) -> FuncT:
     # by the engine
     setattr(method, "_raw_method", True)
     return method
+
+
+def _parse_function_name(fname: str, fobject: Func, target: str | Type[Any], dry: bool = False) -> Func:
+    if is_processable(fobject):
+        if inspect.iscoroutinefunction(fobject):
+            return Backend.run_async(fname, target, dry_run=dry)
+        else:
+            return Backend.run(fname, target, dry_run=dry)
+    else:
+        return fobject
